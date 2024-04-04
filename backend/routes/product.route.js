8fa232dcd704d1eb26813ba955425ed727033b7e
Raad05 <ashabulraad@gmail.com>
2024-04-05 04:02:25 +0600

    defined updateProduct route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,12 +3,14 @@ import {
   createProduct,
   deleteProduct,
   getProducts,
+  updateProduct,
 } from "../controller/product.controller.js";
 
 const router = Router();
 
-router.get("/getProducts", getProducts);
 router.post("/createProduct", createProduct);
+router.get("/getProducts", getProducts);
+router.put("/updateProduct/:id", updateProduct);
 router.delete("/deleteProduct/:id", deleteProduct);
 
 export default router;
