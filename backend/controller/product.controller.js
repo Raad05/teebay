5ddf81cadf2943e46f5063dba63f365d3420a14e
Raad05 <ashabulraad@gmail.com>
2024-04-05 00:00:04 +0600

    created getProducts API

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -17,3 +17,16 @@ export const createProduct = async (req, res) => {
     res.json({ status: 400, message: "Failed to create product.", error: e });
   }
 };
+
+export const getProducts = async (req, res) => {
+  try {
+    const result = await prisma.product.findMany({});
+
+    res.json({
+      status: 200,
+      data: result,
+    });
+  } catch (e) {
+    res.json({ status: 400, message: "Failed to fetch products.", error: e });
+  }
+};
