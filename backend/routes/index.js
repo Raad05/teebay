28996a1b658de9a11648a65f52f562584fc3587c
Raad05 <ashabulraad@gmail.com>
2024-04-05 01:06:50 +0600

    added ProductRoutes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserRoutes from "./user.route.js";
+import ProductRoutes from "./product.route.js";
 
 const router = Router();
 
 router.use("/api/v1/user", UserRoutes);
+router.use("/api/v1/product", ProductRoutes);
 
 export default router;
