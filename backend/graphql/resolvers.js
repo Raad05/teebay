ce1fad9f163fb2d134ad4578337cba99d5f50d4c
Raad05 <ashabulraad@gmail.com>
2024-04-07 09:35:26 +0600

    refactored server configuration

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -7,7 +7,7 @@ import { createUser, loginUser } from "../controller/user.controller.js";
 const resolvers = {
   // queries
   Query: {
-    products: async (_, __, ___) => {
+    products: async () => {
       try {
         const response = await getAllProducts();
 
@@ -20,7 +20,7 @@ const resolvers = {
 
   // mutations
   Mutation: {
-    createUser: async (_, { input }, __) => {
+    createUser: async (_, { input }) => {
       try {
         const response = await createUser(input);
         return response;
@@ -28,7 +28,7 @@ const resolvers = {
         throw new Error(e);
       }
     },
-    createProduct: async (_, { input }, __) => {
+    createProduct: async (_, { input }) => {
       try {
         const response = await createProduct(input);
         return response;
@@ -36,7 +36,7 @@ const resolvers = {
         throw new Error(e);
       }
     },
-    loginUser: async (_, { input }, __) => {
+    loginUser: async (_, { input }) => {
       try {
         const response = await loginUser(input);
 
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,24 @@ import express from "express";
 import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-import typeDefs from "./graphql/typeDefs.js";
 import resolvers from "./graphql/resolvers.js";
+import { readFileSync } from "fs";
+import gql from "graphql-tag";
+import { buildSubgraphSchema } from "@apollo/subgraph";
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+const typeDefs = gql(
+  readFileSync("./graphql/schema.graphql", {
+    encoding: "utf-8",
+  })
+);
+
 const main = async () => {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: buildSubgraphSchema({ typeDefs, resolvers }),
   });
 
   await server.start();
