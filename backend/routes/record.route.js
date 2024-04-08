1e702c587c4f439cc0338b1cee2b047ba6e1eed2
Raad05 <ashabulraad@gmail.com>
2024-04-08 16:26:30 +0600

    deleted routes

--- backend/routes/record.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createRecord, getRecords } from "../controller/record.controller.js";
-
-const router = Router();
-
-router.post("/createRecord", createRecord);
-router.get("/records", getRecords);
-
-export default router;
--- backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser, loginUser } from "../controller/user.controller.js";
-
-const router = Router();
-
-router.post("/signup", createUser);
-router.post("/login", loginUser);
-
-export default router;
