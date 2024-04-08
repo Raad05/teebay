c531ae482eaf2e55bd570ebf1fce3eb3bf424cc2
Raad05 <ashabulraad@gmail.com>
2024-04-08 21:06:52 +0600

    ProductDetails conditional rendering

--- frontend/src/components/ProductDetails/ProductDetails.jsx
+++ frontend/src/components/ProductDetails/ProductDetails.jsx
@@ -5,6 +5,7 @@ const PRODUCT = gql`
   query GetProduct($id: Int!) {
     product(id: $id) {
       name
+      userId
       description
       categories
       sellingPrice
@@ -24,9 +25,15 @@ const ProductDetails = () => {
   if (loading) return <p>Loading...</p>;
 
   const product = data.product;
+  const userId = JSON.parse(localStorage.getItem("user")).id;
 
   return (
     <div className="product-details w-1/2 mx-auto mt-20">
+      {product.userId === userId && (
+        <p className="text-2xl text-center text-green-500 my-10 font-bold">
+          ThIS PRODUCT IS OWNED BY YOU
+        </p>
+      )}
       <p className="text-xl font-bold">{product.name}</p>
       <p className="text-gray-500">
         Categories:{" "}
@@ -42,14 +49,16 @@ const ProductDetails = () => {
         Price: ${product.sellingPrice} | Rent: ${product.rentingPrice}
       </p>
       <p className="my-3">{product.description}</p>
-      <div className="flex justify-end my-10">
-        <button className="m-3 text-white bg-purple-600 px-4 py-2 rounded">
-          Buy
-        </button>
-        <button className="m-3 text-white bg-purple-600 px-4 py-2 rounded">
-          Rent
-        </button>
-      </div>
+      {product.userId !== userId && (
+        <div className="flex justify-end my-10">
+          <button className="m-3 text-white bg-purple-600 px-4 py-2 rounded">
+            Buy
+          </button>
+          <button className="m-3 text-white bg-purple-600 px-4 py-2 rounded">
+            Rent
+          </button>
+        </div>
+      )}
     </div>
   );
 };
