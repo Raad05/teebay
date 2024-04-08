fbe63989cbc06ac2aef7532270a5497f6fd3d7bf
Raad05 <ashabulraad@gmail.com>
2024-04-08 15:36:09 +0600

    rendered user products

--- frontend/src/components/MyProducts/MyProducts.jsx
+++ frontend/src/components/MyProducts/MyProducts.jsx
@@ -1,46 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
 import ProductCard from "../ProductCard/ProductCard";
 
-const products = [
-  {
-    name: "Product 1",
-    description: "Description 1",
-    categories: ["a", "b", "c"],
-    sellingPrice: 50,
-    rentingPrice: 10,
-    createdAt: "Demo",
-    views: 0,
-  },
-  {
-    name: "Product 2",
-    description: "Description 2",
-    categories: ["a", "b", "c"],
-    sellingPrice: 50,
-    rentingPrice: 10,
-    createdAt: "Demo",
-    views: 0,
-  },
-  {
-    name: "Demo",
-    description: "Demo",
-    categories: ["a", "b", "c"],
-    sellingPrice: 50,
-    rentingPrice: 10,
-    createdAt: "Demo",
-    views: 0,
-  },
-];
+const PRODUCTS = gql`
+  query GetAllProducts {
+    products {
+      id
+      userId
+      name
+      description
+      categories
+      sellingPrice
+      rentingPrice
+      createdAt
+      views
+    }
+  }
+`;
 
 const MyProducts = () => {
+  const { data, loading } = useQuery(PRODUCTS);
+  const userId = JSON.parse(localStorage.getItem("user")).id;
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div className="my-products mt-20">
       <h3 className="text-3xl text-center my-10">MY PRODUCTS</h3>
-      {products.map((product, idx) => (
-        <ProductCard
-          key={idx}
-          product={product}
-          path="myProducts"
-        ></ProductCard>
-      ))}
+      {data.products
+        .filter((product) => product.userId === userId)
+        .map((product, idx) => (
+          <ProductCard
+            key={idx}
+            product={product}
+            path="myProducts"
+          ></ProductCard>
+        ))}
     </div>
   );
 };
